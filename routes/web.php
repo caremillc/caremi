5eb924943da0c7f90c30c245cb6da5ee4a073b02
caremi <Willy Nilly>
2025-03-08 23:32:48 +0400

    router middleware group prefix

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,25 @@
 <?php 
 
 use Careminate\Routing\Route;
-use App\Http\Middlewares\Middleware;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Post\PostController;
 
+
 Route::get('/', HomeController::class, 'index',['middleware,admin']);
 // Route::get('/',function(){
-//     return "welcome to web anonymoaus page";
+//     return "welcome to web anonymous page";
 // }, ['middleware,admin']);
 
-Route::get('/about', HomeController::class,'about');
-Route::get('/contact', HomeController::class,'contact');
+Route::group(['prefix'=>'front'], function(){
+    Route::get('/about', HomeController::class,'about');
+});
 
-Route::get('/posts', PostController::class, 'index');
-Route::get('posts/create', PostController::class, 'create');
-Route::post('posts/store', PostController::class, 'store');
-Route::get('posts/{id}/show', PostController::class, 'show');
-Route::get('posts/{id}/edit', PostController::class, 'edit');
-Route::put('posts/{id}/update', PostController::class, 'update');
-Route::delete('posts/{id}/destroy', PostController::class, 'destroy');
+Route::group(['prefix'=>'admin'], function(){
+    Route::get('/posts', PostController::class, 'index');
+    Route::get('posts/create', PostController::class, 'create');
+    Route::post('posts/store', PostController::class, 'store');
+    Route::get('posts/{id}/show', PostController::class, 'show');
+    Route::get('posts/{id}/edit', PostController::class, 'edit');
+    Route::put('posts/{id}/update', PostController::class, 'update');
+    Route::delete('posts/{id}/destroy', PostController::class, 'destroy');
+});
