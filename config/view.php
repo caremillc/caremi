a71fafecd489dc0a720696c61ac3d9fa6de1cc48
caremi <Willy Nilly>
2025-03-09 04:04:30 +0400

    add view cache functionality

--- config/view.php
+++ config/view.php
@@ -1,7 +1,24 @@
 <?php
 
+/**
+ * View Configuration Settings
+ *
+ * This configuration file defines settings related to the handling of views, including the paths for templates,
+ * the caching options, and cache expiry time.
+ * 
+ * Configuration keys:
+ * - `path`: The directory where the view templates are stored.
+ * - `cache_directory`: The directory where the compiled view cache will be stored.
+ * - `cache`: A boolean that controls whether view caching is enabled (true to enable caching, false to disable).
+ * - `cache_expiry`: The time in seconds before the cached views expire.
+ * 
+ * @return array Configuration array for view handling.
+ */
 return [
-    'path' => base_path('templates/views'),
-    'cache_dir'=>storage_path('views/'),  //this
-    'cache'=>true //this
+    'path'            => base_path('templates/views'),        // Define the path to the views directory.
+    'cache_directory' => storage_path('views/'),              // Define the path where view cache files will be stored.
+    'cache'           => false,                                // Set to true to enable view caching.
+    'cache_expiry'    => 360                                   // Set the cache expiry time in seconds (360 seconds = 6 minutes).
 ];
+
+ 
