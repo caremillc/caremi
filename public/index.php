aa3716f397abbb1a137025897b766907e6f81f71
caremi <Willy Nilly>
2025-03-10 19:30:59 +0400

    add request class

--- public/index.php
+++ public/index.php
@@ -3,10 +3,11 @@
 use Dotenv\Dotenv;
 use Careminate\Application;
 use League\Container\Container;
+use Careminate\Http\Requests\Request;
 use App\Providers\LocaleServiceProvider;
 use Careminate\ServiceProviders\LocaleService;
 
-
+define('CAREMI_START', microtime(true));
 define('BASE_PATH', dirname(__DIR__));
 define('ROOT_PATH', __DIR__);
 define('ROOT_DIR', __DIR__);
@@ -19,6 +20,9 @@ require_once BASE_PATH . '/vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
+
+ // Start capturing the current timestamp to calculate processing time
+ $requestStartTime = microtime(true);
 
 // Initialize the HttpKernel to check PHP version and maintenance mode
 // $kernel = new Careminate\Http\HttpKernel();
@@ -36,6 +40,11 @@ $container->add(Application::class, function () use ($container) {
     return new Application($container->get(LocaleService::class));
 });
 
+// request received
+$request = Request::createFromGlobals();
+
+
+
 try {
     // Initialize the application
     $app = $container->get(Application::class);
@@ -49,3 +58,15 @@ try {
     http_response_code(500);
     echo $e->getMessage();
 }
+
+// Calculate the total processing time
+$requestEndTime = microtime(true);
+$executionTime = $requestEndTime - $requestStartTime;
+
+// Log the execution time (for monitoring purposes)
+echo "<pre>";
+echo "Request processed in " . number_format($executionTime, 4) . " seconds.\n";
+
+// Debug: Dump the request
+echo "<pre>";
+dd($request);
