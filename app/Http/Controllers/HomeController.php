d5ecb1f943b3f71112df9fb9fc32cf710e70ab85
caremi <Willy Nilly>
2025-03-10 18:22:08 +0400

    refactor db and model methods

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Careminate\Http\Requests\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {  
-	    // var_dump(request());
-        // var_dump(Request::get('user_id'));
-		// var_dump(request('user_id'));
-        // exit;
-        var_dump(new User());
-        return view('home',['title'=>'Home page']);
+    public function index(Request $request)
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+            echo $user['name'] . "\n";
+        }
+    }
+    public function store(Request $request)
+    {
+        // $user = new User();
+        // $user->name = 'John Doe3';
+        // $user->email = 'john@example.com';
+        // $user->password = 'plaintext'; // Mutator hashes this
+        // $user->save();
+
+        //or 
+
+        $user = User::create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@example.com',
+            'password' => 'secret',
+        ]);
+
+        echo"<pre>";
+        var_dump($user);
+    }
+    public function edit($id)
+    {
+        $user = User::find(1);
+        $user->password = 'new_password'; // Mutator triggers
+        $user->save(); // Saves hashed password
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,4 +6,12 @@ use Careminate\Databases\Model;
 class User extends Model
 {
     protected $table = 'users';
+    protected $fillable = ['name', 'email'];
+    protected $guarded = ['password']; // Explicitly guard password field
+
+    // Automatically hash passwords when setting
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = password_hash($value, PASSWORD_DEFAULT);
+    }
 }
