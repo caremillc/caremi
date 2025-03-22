9e3b90c9d3578fbf428e2d9414106698bbc8e566
caremi <Willy Nilly>
2025-03-22 13:57:12 +0400

    Added Request and Array support class

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1); // public/index.php
 
+use Careminate\Http\Requests\Request;
+
+
 define('CAREMI_START', microtime(true));
 
 // Include Composer autoload to load dependencies
@@ -8,18 +11,17 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 // Capture the current timestamp to calculate the processing time
 $requestStartTime = microtime(true);
 
-// Perform some logic here (e.g., request handling, database query, etc.)
-usleep(100000); // Simulate some processing delay (e.g., database query)
+// request received
+$request = Request::createFromGlobals();
+
+dd($request);
 
 // Calculate the total processing time
 $requestEndTime = microtime(true);
 $executionTime = $requestEndTime - $requestStartTime;
 
-// Send a response
-echo"<pre>";
-echo "Hello World from index.".PHP_EOL;
-
 // Log the execution time (this could be saved to a file or monitored)
-echo"<pre>";
 echo "Request processed in " . number_format($executionTime, 4) . " seconds.\n";
 
+// Send a response
+echo 'Hello World from index';
