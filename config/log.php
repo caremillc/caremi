a858e3485afb3de360f79670f534d32f52be4fc0
caremi <Willy Nilly>
2025-04-13 17:31:36 +0400

    Add Log Exception

---
+++ config/log.php
@@ -0,0 +1,6 @@
+<?php 
+return [
+    'log_file'       => 'error.log', // Default log file
+    'display_errors' => true,       // Whether to display errors
+    'log_to_file'    => true,      // Whether to log to file
+];
