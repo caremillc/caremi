9d1e32ab2425ce59e4c12d2e26119e733a5c4f50
caremi <Willy Nilly>
2025-06-07 13:04:30 +0400

    Response & Redirect Classes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php declare(strict_types=1);
 
+use Careminate\Http\Responses\Response;
+
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
-echo "Hello World";
+// Default usage (with buffering)
+$response = new Response('Hello World');
+$response->send();
+
+// dd($response);
+
+$response->send();
