b99a1e0d904c09a80591f433fa105c23dfec9b62
caremi <Willy Nilly>
2025-06-21 21:19:54 +0400

    add default route params

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -35,6 +35,8 @@ class PostController extends Controller
 
     public function show($id)
     {
+        echo $id;
+        exit;
         $post = Post::findOrFail($id); // Careminate must support or handle fallback
         return view('posts.show', compact('post'));
     }
--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,10 @@ Route::get('/posts/create', [PostController::class, 'create'])->name('posts.crea
 Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{id}/update', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/{id}/show', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/posts/{id}/show', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{id}/show', [PostController::class, 'show'])->defaults(['id' => 1]); // route with  default value
 Route::delete('/posts/{id}/delete', [PostController::class, 'destroy'])->name('posts.destroy');
 
 // posts resource
 Route::resource('posts', [PostController::class]);
+
