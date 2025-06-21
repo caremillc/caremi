0471e2efa99e9ead5a7bab97107a288d1a230d1d
caremi <Willy Nilly>
2025-06-21 17:02:56 +0400

    Anonymous & invoke routes

---
+++ app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use Careminate\Http\Requests\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all(); // or paginate if implemented
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        Post::create($validated);
+
+        return redirect('/posts'); // simple redirect
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id); // Careminate must support or handle fallback
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($validated);
+
+        return redirect('/posts');
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return redirect('/posts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,30 @@
 
 use Careminate\Routing\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvokeController;
+use App\Http\Controllers\Post\PostController;
 
 // Closure route
 Route::get('/', function () {
-    return 'anonymous route is working!';
+    return 'Test route is working!';
 });
 
+// __invoke()
+Route::get('/invoke', InvokeController::class); 
+
 // Controller routes
-Route::get('/home', HomeController::class, 'index');
-Route::get('/about', HomeController::class, 'about');
-Route::get('/article/{id}/{slug}', HomeController::class, 'article');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+// posts
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{id}/update', [PostController::class, 'update']);
+Route::get('/posts/{id}/show', [PostController::class, 'show']);
+Route::delete('/posts/{id}/delete', [PostController::class, 'destroy']);
+
+// posts resource
+Route::resource('posts', [PostController::class]);
+
+?>
