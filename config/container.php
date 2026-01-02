0cdcf455e13c8dc22c841a1314a2f2718da6d1f5
caremi <Willy Nilly>
2026-01-02 14:35:42 +0400

    Twig & Reusable Templates

--- config/container.php
+++ config/container.php
@@ -2,7 +2,6 @@
 // Load environment variables
 $dotenv = new \Symfony\Component\Dotenv\Dotenv();
 $dotenv->load(dirname(__DIR__) . '/.env');
-
 /**
  * --------------------------------------------------------------------------
  * Service Container Configuration
@@ -23,11 +22,13 @@ $container = new \League\Container\Container();
 // Enable auto-resolution of dependencies through reflection
 $container->delegate(new \League\Container\ReflectionContainer(true));
 
-
 #env parameters
 $appEnv = env('APP_ENV', 'production'); // Default to 'production' if not set
 $appKey = env('APP_KEY'); // Default to 'production' if not set
 $appVersion = env('APP_VERSION');
+
+# template
+$templatesPath = BASE_PATH . '/templates/views';
 
 $container->add('APP_ENV', new \League\Container\Argument\Literal\StringArgument($appEnv));
 $container->add('APP_KEY', new \League\Container\Argument\Literal\StringArgument($appKey));
@@ -49,8 +50,28 @@ $routes = include BASE_PATH . '/routes/web.php';
 $container->extend(Careminate\Routing\RouterInterface::class)
           ->addMethodCall('setRoutes',[new League\Container\Argument\Literal\ArrayArgument($routes)]);
 
+ //  twig tempalte
+$container->addShared('filesystem-loader', \Twig\Loader\FilesystemLoader::class)
+    ->addArgument(new \League\Container\Argument\Literal\StringArgument($templatesPath));
 
+// Register the Twig Environment as a shared (singleton) instance
+// and inject the 'filesystem-loader' service into its constructor.
+$container->addShared('twig', \Twig\Environment::class)
+    ->addArgument('filesystem-loader');
+
+// Register the AbstractController so it can be resolved by the container.
+$container->add(\Careminate\Http\Controllers\AbstractController::class);
+
+// Automatically call the setContainer() method on any class that extends AbstractController
+// This injects the container itself into the controller, enabling dependency resolution within controllers.
+$container->inflector(\Careminate\Http\Controllers\AbstractController::class)
+    ->invokeMethod('setContainer', [$container]);
+// end twig template
 // Debug output (should be removed in production)
-dd($container);
+// dd($container);
 
 return $container;
+
+
+
+
