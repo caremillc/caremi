fb43685d395b7fbb852e48e70619e406c638fb18
caremi <Willy Nilly>
2026-02-22 09:34:10 +0400

    Add env variables to twig template

--- config/app.php
+++ config/app.php
@@ -1,7 +1,19 @@
 <?php declare (strict_types = 1);
 
 return [
-
-    'debug' => env('APP_DEBUG', false), # Whether to enable debug mode (true/false)
-
+    'name'            => env('APP_NAME', 'Caremi'),
+    'version'         => env('APP_VERSION', '1.0.0'), // this
+    'env'             => env('APP_ENV', 'production'),
+    'debug'           => (bool) env('APP_DEBUG', false), # Whether to enable debug mode (true/false)
+    'url'             => env('APP_URL', 'http://localhost:8000'),
+    'asset_url'       => env('ASSET_URL'),
+    'timezone'        => 'UTC',
+    'locale'          => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale'    => 'en_US',
+    'key'             => env('APP_KEY'),
+    'cipher'          => 'AES-256-CBC',
+    'maintenance'     => [
+        'driver' => 'file',
+    ],
 ];
