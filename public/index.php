892453f1378d3f3ccfad621adb05d07cd3cb5b1a
caremi <Willy Nilly>
2026-03-28 11:14:06 +0400

    add container class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,37 @@
 <?php declare(strict_types=1);
-require_once __DIR__.'/../bootstrap/app.php';
+
+// require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 require BASE_PATH . '/bootstrap/performance.php';
 
-echo 'Hello work';
+
+/*
+|--------------------------------------------------------------------------
+| Handle Request Through Kernel
+|--------------------------------------------------------------------------
+*/
+// $request = Request::capture();
+
+// dd($request);
+
+// $kernel = $app->make(HttpKernel::class);
+
+// dd($kernel);
+
+// $response = $kernel->handle($request);
+
+// $response->send();
+
+// $kernel->terminate($request, $response);
+
+
+// dd($response);
+// dd($request);
+/*
+|--------------------------------------------------------------------------
+| Send Response
+|--------------------------------------------------------------------------
+*/
+
+// $response->send();
